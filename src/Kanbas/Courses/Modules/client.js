c614d975218d6c2fc01a24b3fd7cf18d853ec488
Liang jimenez <liangjimenez@yahoo.com>
2023-11-20 16:49:02 -0500

    api

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,11 +1,7 @@
 import axios from "axios";
-const COURSES_URL = "http://localhost:4000/api/courses";
-const MODULES_URL =
-  "https://kanbas-node-server-app-oigt.onrender.com/api/modules";
-// const MODULES_URL = "http://localhost:4000/api/modules";
-// const API_BASE = process.env.REACT_APP_API_BASE;
-// const MODULES_URL = `${API_BASE}/modules`;
-
+const COURSES_URL = `${API_BASE}/courses`;
+const API_BASE = "https://kanbas-node-server-app-oigt.onrender.com/api";
+const MODULES_URL = `${API_BASE}/modules`;
 export const updateModule = async (module) => {
   const response = await axios.put(`${MODULES_URL}/${module._id}`, module);
   return response.data;
