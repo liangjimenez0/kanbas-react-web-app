bf677c36ec326e4a15db84f54f4c9916e666684f
Liang jimenez <liangjimenez@yahoo.com>
2023-11-20 17:42:25 -0500

    new

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -12,7 +12,7 @@ import axios from "axios";
 
 function Courses() {
   const { courseId } = useParams();
-  const URL = "http://localhost:4000/api/courses";
+  const URL = "https://kanbas-node-server-app-oigt.onrender.com/api/courses";
   const [course, setCourse] = useState({
     name: "New Course",
   });
