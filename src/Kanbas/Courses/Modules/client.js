50261af0ef3ebd2e352c5b21f386a6bfa362d3aa
Liang jimenez <liangjimenez@yahoo.com>
2023-11-20 17:05:15 -0500

    change links for api

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -3,24 +3,31 @@ const COURSES_URL = `${API_BASE}/courses`;
 const API_BASE = "https://kanbas-node-server-app-oigt.onrender.com/api";
 const MODULES_URL = `${API_BASE}/modules`;
 export const updateModule = async (module) => {
-  const response = await axios.put(`${MODULES_URL}/${module._id}`, module);
+  const response = await axios.put(
+    `https://kanbas-node-server-app-oigt.onrender.com/api/modules/${module._id}`,
+    module
+  );
   return response.data;
 };
 
 export const deleteModule = async (moduleId) => {
-  const response = await axios.delete(`${MODULES_URL}/${moduleId}`);
+  const response = await axios.delete(
+    `https://kanbas-node-server-app-oigt.onrender.com/api/modules/${moduleId}`
+  );
   return response.data;
 };
 
 export const createModule = async (courseId, module) => {
   const response = await axios.post(
-    `${COURSES_URL}/${courseId}/modules`,
+    `https://kanbas-node-server-app-oigt.onrender.com/api/courses/${courseId}/modules`,
     module
   );
   return response.data;
 };
 
 export const findModulesForCourse = async (courseId) => {
-  const response = await axios.get(`${COURSES_URL}/${courseId}/modules`);
+  const response = await axios.get(
+    `https://kanbas-node-server-app-oigt.onrender.com/api/courses/${courseId}/modules`
+  );
   return response.data;
 };
